c494449ec9865a583394e2f03dc8e988d43566b2
Pedro Alvito <pedro.alvito@tobii.com>
2016-12-12 16:58:25 +0100

    2015: Problem 7

---
+++ 2015/7B.py
@@ -0,0 +1,51 @@
+with open('7.in','r') as f:
+	strings = f.read().split('\n')
+
+wires = {'b':46065}
+
+i = 0
+
+def get_value(v):
+	return wires[v] if v in wires else int(v)
+
+def is_valid(v):
+	return v in wires or v.isdigit()
+
+while len(strings) > 0:
+	if i >= len(strings): i = 0
+
+	c = strings[i].split('->')
+	w = c[1].strip()
+
+	if w == 'b':
+		strings.remove(strings[i])
+		continue
+
+	op = c[0].strip().split()
+	if len(op) == 1 and is_valid(op[0]):
+		wires[w] = get_value(op[0])
+		strings.remove(strings[i])
+	elif len(op) > 1:
+		if 'AND' == op[1]:
+			if is_valid(op[0]) and is_valid(op[2]):
+				wires[w] = get_value(op[0]) & get_value(op[2])
+				strings.remove(strings[i])
+		elif 'OR' == op[1]:
+			if is_valid(op[0]) and is_valid(op[2]):
+				wires[w] = get_value(op[0]) | get_value(op[2])
+				strings.remove(strings[i])
+		elif 'LSHIFT' == op[1]:
+			if is_valid(op[0]):
+				wires[w] = get_value(op[0]) << get_value(op[2])
+				strings.remove(strings[i])
+		elif 'RSHIFT' == op[1]:
+			if is_valid(op[0]):
+				wires[w] = get_value(op[0]) >> get_value(op[2])
+				strings.remove(strings[i])
+		elif 'NOT' == op[0]:
+			if is_valid(op[1]):
+				wires[w] = ~get_value(op[1])
+				strings.remove(strings[i])
+	i+=1
+
+print wires['a']
