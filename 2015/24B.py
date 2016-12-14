9437ddf6b169263d38b18e88ef85ddefd7933bf2
Pedro Alvito <pedro.alvito@tobii.com>
2016-12-14 16:34:02 +0100

    2015: Problem 24

--- 2015/24B.py
+++ 2015/24B.py
@@ -5,7 +5,7 @@ from operator import mul
 with open('24.in') as f:
 	numbers = [int(x) for x in f.read().split('\n')]
 
-group_size = sum(numbers) // 3
+group_size = sum(numbers) // 4
 
 for i in xrange(group_size):
     qe = [reduce(mul, c) for c in combinations(numbers, i) if sum(c) == group_size]
