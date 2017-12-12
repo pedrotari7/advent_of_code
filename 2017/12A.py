e759a1e9732aca25eea35e8be271f852e5167068
pedrotari7 <pedrotari7@gmail.com>
2017-12-12 07:34:42 +0100

    2017: Problem 12

--- 2017/12A.py
+++ 2017/12A.py
@@ -1,12 +1,10 @@
 net = {int(n[0]):[int(_.strip(',')) for _ in n[2:]] for n in map(str.split,open('12.in'))}
 
-def group(n):
-    visited = set()
-    to_visit = set([n])
-    while to_visit:
-        c = to_visit.pop()
-        visited.add(c)
-        to_visit = to_visit.union(set([ch for ch in net[c] if ch not in visited]))
-    return visited
+visited = set()
+to_visit = set([0])
+while to_visit:
+    c = to_visit.pop()
+    visited.add(c)
+    to_visit = to_visit.union(set([ch for ch in net[c] if ch not in visited]))
 
-print len(group(0))
+print len(visited)
--- 2017/12B.py
+++ 2017/12B.py
@@ -1,20 +1,15 @@
 net = {int(n[0]):[int(_.strip(',')) for _ in n[2:]] for n in map(str.split,open('12.in'))}
 
-def group(n):
-    visited = set()
+visited = set()
+groups = 0
+for n in net:
+    if n in visited: continue
+    groups+=1
     to_visit = set([n])
     while to_visit:
         c = to_visit.pop()
         visited.add(c)
         to_visit = to_visit.union(set([ch for ch in net[c] if ch not in visited]))
-    return visited
 
-groups = []
-
-for s in net:
-    new_group = group(s)
-    if not any((bool(set(new_group) & set(g)) for g in groups)):
-            groups.append(new_group)
-
-print len(groups)
+print groups
 
