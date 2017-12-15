754f2aba78b23c3dc156e2036da20de5a2fa86cd
pedrotari7 <pedrotari7@gmail.com>
2017-12-15 10:08:42 +0100

    2017: Problem 15

--- 2017/15B.py
+++ 2017/15B.py
@@ -1,9 +1,9 @@
-def g(n, k, m=1):
-    while 1:
+def g(n, k, m):
+    i = 0
+    while i < 5*10**6:
         n = n*k%2147483647
         if n%m==0:
+            i+=1
             yield n&(2**16-1)
 
-a,b = g(783, 16807, 4), g(325, 48271, 8) 
-
-print sum(next(a)==next(b) for _ in xrange(5*10**6))
+print sum(a==b for a,b in zip(g(783,16807,4),g(325,48271,8)))
