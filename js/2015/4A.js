8b804190a77e13e422ffe3493e733849aeb3fd09
Pedro Alvito <pedrotari7@gmail.com>
2019-12-02 21:52:23 +0100

    2015js: Problem 4

---
+++ js/2015/4A.js
@@ -0,0 +1,13 @@
+const { getDataFromFile, md5 } = require('../utilities');
+
+let key = getDataFromFile(4);
+
+const zeros = (str, n) => str.startsWith('0'.repeat(n));
+
+let i = 0;
+
+while (!zeros(md5(`${key}${i}`), 5)) {
+  i += 1;
+}
+
+console.log(i)
