f9548ffd373c85aa7d526cc250bfb8e847fa2aef
Pedro Alvito <pedrotari7@gmail.com>
2022-12-18 22:37:29 +0000

    2022: Problem 18

--- ts/2022/18.ts
+++ ts/2022/18.ts
@@ -1,46 +1,24 @@
-import { timer, getIntMatrixFromFile, int10 } from '../utilities.ts';
+import { timer, getIntMatrixFromFile, inRange } from '../utilities.ts';
 
 timer.start();
 
+type N = number;
 class Array3D {
-  private elements: Record<number, Record<number, Set<number>>> = {};
-  add = (x: number, y: number, z: number) => {
-    this.elements = {
-      ...this.elements,
-      [x]: { ...this.elements[x], [y]: ((this.elements[x] ?? {})[y] ?? new Set()).add(z) },
-    };
+  public min = Infinity;
+  public max = -Infinity;
+
+  private elements = new Set<string>();
+  add = (x: N, y: N, z: N) => {
+    this.min = Math.min(this.min, x, y, z);
+    this.max = Math.max(this.max, x, y, z);
+    this.elements.add([x, y, z].toString());
     return this;
   };
 
-  has = (x: number, y: number, z: number) =>
-    Object.keys(this.elements).includes(`${x}`) &&
-    Object.keys(this.elements[x]).includes(`${y}`) &&
-    this.elements[x][y].has(z);
-}
+  has = (x: N, y: N, z: N) => this.elements.has([x, y, z].toString());
 
-const cubes = getIntMatrixFromFile(18, ',');
-
-const lava = cubes.reduce((lava, [x, y, z]) => lava.add(x, y, z), new Array3D());
-
-console.log('cubes', cubes);
-
-console.log('lava', lava);
-
-let p1 = 0;
-
-for (const [x, y, z] of cubes) {
-  console.log('p1,x,y,z', p1, x, y, z);
-
-  console.log(
-    lava.has(x + 1, y, x),
-    lava.has(x - 1, y, z),
-    lava.has(x, y + 1, z),
-    lava.has(x, y - 1, z),
-    lava.has(x, y, z + 1),
-    lava.has(x, y, z - 1)
-  );
-  p1 +=
-    +!lava.has(x + 1, y, x) +
+  area = (x: N, y: N, z: N) =>
+    +!lava.has(x + 1, y, z) +
     +!lava.has(x - 1, y, z) +
     +!lava.has(x, y + 1, z) +
     +!lava.has(x, y - 1, z) +
@@ -48,7 +26,32 @@ for (const [x, y, z] of cubes) {
     +!lava.has(x, y, z - 1);
 }
 
-const p2 = null;
+const cubes = getIntMatrixFromFile(18, ',');
+
+const lava = cubes.reduce((lava, [x, y, z]) => lava.add(x, y, z), new Array3D());
+
+const p1 = cubes.reduce((c, [x, y, z]) => c + lava.area(x, y, z), 0);
+
+const visited = new Set();
+
+let p2 = 0;
+const Q = [[lava.min - 1, lava.min - 1, lava.min - 1]];
+
+while (Q.length) {
+  const [x, y, z] = Q.shift()!;
+  if (visited.has(`${x},${y},${z}`) || lava.has(x, y, z)) continue;
+  if (
+    !inRange(x, lava.min - 1, lava.max + 1) ||
+    !inRange(y, lava.min - 1, lava.max + 1) ||
+    !inRange(z, lava.min - 1, lava.max + 1)
+  )
+    continue;
+  visited.add(`${x},${y},${z}`);
+
+  p2 += 6 - lava.area(x, y, z);
+
+  Q.push([x + 1, y, z], [x - 1, y, z], [x, y + 1, z], [x, y - 1, z], [x, y, z + 1], [x, y, z - 1]);
+}
 
 console.log('p1', p1);
 console.log('p2', p2);
