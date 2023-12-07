3fe5d3a4a2111326d1d618934e18c663ffce1f7a
Pedro Alvito <pedroalvito@spotify.com>
2023-12-07 21:39:33 +0100

    2021: Problem 7 refactor

---
+++ ts/2021/7.ts
@@ -0,0 +1,26 @@
+import { getIntArrayFromFile, timer, max, sum, range } from '../utilities.ts';
+
+timer.start();
+const data = getIntArrayFromFile(7, ',');
+
+const findBest = (fn: (n: number) => number) => {
+  return [...range(0, max(data))].reduce(
+    ({ b, i }, c) => {
+      const fuel = sum(data.map(d => fn(Math.abs(d - c))));
+      return fuel < b ? { b: fuel, i: c } : { b, i };
+    },
+    { b: 2 ** 32, i: 0 }
+  ).b;
+};
+
+console.log(
+  `p1`,
+  findBest(n => n)
+);
+
+console.log(
+  `p2`,
+  findBest(n => (n * (n + 1)) / 2)
+);
+
+timer.stop();
